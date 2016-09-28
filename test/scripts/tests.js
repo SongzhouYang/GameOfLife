3032f6945b6b3188423188339e2d2f3b27115a9d
Songzhou Yang <yangsongzhou@outlook.com>
2016-09-28 10:29:49 +0800

    modify test

--- test/scripts/tests.js
+++ test/scripts/tests.js
@@ -4,20 +4,20 @@
     it('should be a object', function () {
       assert.isObject(gol);
     });
-    it('should have property canvasSize ', function () {
-      expect(gol).to.have.property('canvasSize');
+    it('should have keys canvasSize ', function () {
+      expect(gol).to.include.keys('canvasSize');
     });
-    it('should have property canvasGrid ', function () {
-      expect(gol).to.have.property('canvasGrid');
+    it('should have keys canvasGrid ', function () {
+      expect(gol).to.include.keys('canvasGrid');
     });
-    it('should have property period ', function () {
-      expect(gol).to.have.property('period');
+    it('should have keys period ', function () {
+      expect(gol).to.include.keys('period');
     });
-    it('should have property initProbability ', function () {
-      expect(gol).to.have.property('initProbability');
+    it('should have keys initProbability ', function () {
+      expect(gol).to.include.keys('initProbability');
     });
-    it('should have property cellSize ', function () {
-      expect(gol).to.have.property('cellSize');
+    it('should have keys cellSize ', function () {
+      expect(gol).to.include.keys('cellSize');
     });
   });
 
@@ -25,14 +25,69 @@
     it('should be a function', function () {
       assert.isFunction(Cell);
     });
-    it('should have a parameter', function () {
+    it('should have one parameter', function () {
       expect(Cell).to.have.lengthOf(1);
     });
-    it('should have prototype init', function () {
-      expect(Cell).to.respondTo('init');
+    context('Cell.prototype.init', function () {
+      it('should have prototype init', function () {
+        expect(Cell).to.respondTo('init');
+      });
+      it('should have one parameter', function () {
+        expect(Cell.prototype.init).to.have.lengthOf(1);
+      })
     });
-    it('should have prototype draw', function () {
-      expect(Cell).to.respondTo('draw');
+    context('Cell.prototype.draw', function () {
+      it('should have prototype draw', function () {
+        expect(Cell).to.respondTo('draw');
+      });
+      it('should have two parameters', function () {
+        expect(Cell.prototype.draw).to.have.lengthOf(2);
+      });
+    });
+  });
+
+  describe('canvasInit', function () {
+    it('should be a function', function () {
+      assert.isFunction(canvasInit);
+    });
+    it('should have one parameter', function () {
+      expect(canvasInit).to.have.lengthOf(1);
+    });
+    it('should return an object', function () {
+      expect(typeof canvasInit(gol)).to.be.deep.equal('object');
+      document.body.removeChild(document.querySelector('#canvas'));
+    });
+  });
+
+  describe('aliveCellCal', function () {
+    it('should be a function', function () {
+      assert.isFunction(aliveCellCal);
+    });
+    it('should have four parameters', function () {
+      expect(aliveCellCal).to.have.lengthOf(4);
+    });
+    it('should return a number', function () {
+      var cell = new Cell(gol);
+      cell.init(gol);
+      expect(typeof aliveCellCal(cell.states, 1, 1, gol)).to.be.deep.equal('number');
+    });
+  });
+
+  describe('animate', function () {
+    it('should be a function', function () {
+      assert.isFunction(animate);
+    });
+    it('should have three parameters', function () {
+      expect(animate).to.have.lengthOf(3);
+    });
+  });
+
+  describe('start', function () {
+    it('should be a function', function () {
+      assert.isFunction(start);
+    });
+    it('should have one parameter', function () {
+      expect(start).to.have.lengthOf(1);
     });
   });
 })();
